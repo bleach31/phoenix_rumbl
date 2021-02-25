6cf1d5406fda7c9824bd712e15a7e74a6dcfc746
Motohiro <c201075@gmail.com>
2021-02-25 03:00:41 +0000

    add change user functions

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -11,8 +11,9 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
-  alias Rumbl.Repo
   alias Rumbl.Accounts.User
+
+  alias Rumbl.Repo
 
   def get_user(id) do
     Repo.get(User, id)
@@ -28,5 +29,9 @@ defmodule Rumbl.Accounts do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
   end
 end
