aa8ce5977746ff1086b4d25b8256d35aaf8d16b5
DNMotoShiba <motohiro.shibakawa.j7r@jp.denso.com>
2022-02-10 02:08:52 +0000

    add comment

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -43,7 +43,7 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
-    |> unique_constraint(:username)
+    |> unique_constraint(:username) # こいつを付けたところで何も起きない（逆に外すとエラーは確認できる）。ランディングページがない。
   end
 
 
