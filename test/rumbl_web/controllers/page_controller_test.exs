d02163abf0b3f4d728a78c2a30e53b18301bed37
DNMotoShiba <motohiro.shibakawa.j7r@jp.denso.com>
2022-03-24 02:01:20 +0000

    integrated test

--- test/rumbl_web/controllers/page_controller_test.exs
+++ test/rumbl_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule RumblWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
+    # =~ 文字列を含むか否か
+    # https://hexdocs.pm/elixir/Kernel.html#=~/2
     assert html_response(conn, 200) =~ "Welcome to Rumbl.io!"
   end
 end
