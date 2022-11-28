02f7ff6e68a8cc9fab1eddf9cef40ae6b574c34f
Motohiro <c201075@gmail.com>
2022-11-28 10:01:17 +0000

    p205

--- assets/js/video.js
+++ assets/js/video.js
@@ -14,7 +14,7 @@ let Video = {
     let playerId = element.getAttribute("data-player-id")
     let videoId  = element.getAttribute("data-id")
     socket.connect()
-    Player.init(element.id, playerId, () => { 
+    Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket)
     })
   },
@@ -24,11 +24,40 @@ let Video = {
     let msgInput     = document.getElementById("msg-input")
     let postButton   = document.getElementById("msg-submit")
     let vidChannel   = socket.channel("videos:" + videoId)
-    // TODO join the vidChannel
-    // ここでsocketになんかを与えたりする気がする
+
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e) )
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      console.log("here is on")
+      this.renderAnnotation(msgContainer, resp)
+    })
+
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
-      .receive("error", reason => console.log("join failed", reason) ) 
+      .receive("ok", resp => console.log("joined the video channel", resp) )
+      .receive("error", reason => console.log("join failed", reason) )
+  },
+
+  esc(str){ 
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){ 
+    let template = document.createElement("div")
+
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 export default Video
