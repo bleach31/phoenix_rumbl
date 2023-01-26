122f081c67d1380ceec65123cd933671bb5e999b
Motohiro <c201075@gmail.com>
2023-01-26 10:00:07 +0000

    207 動かない。

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,23 +1,28 @@
+#---
+# Excerpted from "Programming Phoenix 1.4",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix14 for more book information.
+#---
 defmodule RumblWeb.Auth do
   import Plug.Conn
   import Phoenix.Controller
+
   alias RumblWeb.Router.Helpers, as: Routes
 
-  # コンパイル時にオプションを指定する
   def init(opts), do: opts
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
 
     cond do
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
 
-      # &&演算子は、結果が真であれば右オペランドの値が返ってくる
-      # nil,false以外の値は真して、扱われる
-      # この場合、user_idが真なら、get_userの結果がuserに入る
       user = user_id && Rumbl.Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -26,14 +31,23 @@ defmodule RumblWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
-    |> configure_session([renew: true])
+    |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
     configure_session(conn, drop: true)
   end
+
 
   def authenticate_user(conn, _opts) do
     if conn.assigns.current_user do
@@ -45,5 +59,4 @@ defmodule RumblWeb.Auth do
       |> halt()
     end
   end
-
 end
